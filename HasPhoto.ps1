b4a1e7375255a48658aef74338a800e1d71c1f68
basher81 <61800564+basher81@users.noreply.github.com>
2020-09-28 17:10:33 -0400

    HasPhoto
    Exports CSV to c:\temp that shows who has a photo associated with their Azure/Office 365 account

---
+++ HasPhoto.ps1
@@ -0,0 +1,27 @@
+Connect-AzureAD
+$outfile = "C:\temp\UserProfilePic_Report.csv"
+    
+ $users = Get-AzureADUser -All $true
+ $results = foreach($user in $users){
+     $userName = $user.DisplayName
+     try{
+         $profilePics = Get-AzureADUserThumbnailPhoto -ObjectId $user.ObjectId
+         if($profilePics -ne $null)
+         {
+             foreach($profilePic in $profilePics){
+                Write-Host "User $userName has profile pic" -ForegroundColor Green
+                $output = @($userName + ", Present")
+                $output | Out-File -FilePath $outfile -Append -Force 
+     
+             }
+         }
+            
+     } catch{
+         Write-Host "User $userName doesnt have a profile pic"  -ForegroundColor Red
+         $output = @($userName + ", Not_Present")
+         $output | Out-File -FilePath $outfile -Append -Force 
+     }
+        
+ }
+    
+ Import-Csv -Path $outfile
