b307265a2388c1bc4531bd9584c2551945c7a3e9
basher81 <61800564+basher81@users.noreply.github.com>
2020-09-30 15:29:36 -0400

    Update RemovePhotoBasedOnLicenseType.ps1

--- RemovePhotoBasedOnLicenseType.ps1
+++ RemovePhotoBasedOnLicenseType.ps1
@@ -1,3 +1,2 @@
- $Users = Get-AzureADUser | Where {$_.UserType -eq 'Member' -and $_.AssignedLicenses -ne 'EnterprisePack'}
->>
->> foreach ($user in $Users) { Remove-UserPhoto -Identity $user.UserPrincipalName -Confirm:$false} 
+﻿$Users = Get-User -ResultSize unlimited | Where {$_.UserType -eq 'Member' -and $_.AssignedLicenses -ne 'EnterprisePack'};
+foreach ($user in $Users ){Remove-UserPhoto -Identity $user.UserPrincipalName -Confirm:$false}
